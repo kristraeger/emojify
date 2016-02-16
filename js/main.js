509f3c98143949173ed7d4f7581e281b90630968
Kristina Traeger <kristina@traeger.co>
2016-02-15 17:58:45 -0800

    still working

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
+function load(){
+
+$.ajax({
+	url: "http://www.kristinawritesco.de/emojify/js/emojis.json",
+	dataType: "jsonp",
+	success: function(data) {
+	console.log(data)
+	},
+	type: "GET"
+})
+}
+
+
 function emojify() {
 	
-	// get input text and split in into words
-	var input = getElementById("input").text().split()
+	// get input text and split into words
+	var input = []
+	input = String(document.getElementById("input").value).split(/(\s+)/)
 	console.log(input)
 
-	// get data from local json file
-	$.ajax({
-		url: "https://github.com/kristraeger/emojify/blob/master/js/emojis.json",
-		dataType: "json",
-		success: function (emojis) {
-			console.log(emojis.keywords)
-		}
-
-	})
 
 } // ./ emojify
-
 
 
